13b034560931775c556770baf5a7346f9bf628b6
oldmcdonnell <oldmcdonnell@gmail.com>
2024-02-16 14:17:25 -0500

    updated webapp for SQL option

--- webapp.py
+++ webapp.py
@@ -5,11 +5,22 @@ import sqlite3
 
 #Esatblish a conneciton and a server
 connection = sqlite3.connect("data.db")
-cursor = connection.cursor
+cursor = connection.cursor()
+cursor.execute("SELECT date FROM temp")
+date = cursor.fetchall()
+date = [item[0] for item in date]
 
-df = pandas.read_csv("data.txt")
+cursor.execute("SELECT temperature FROM temp")
+temperature = cursor.fetchall()
+temperature = [item[0] for item in temperature]
 
-figure = px.line(x=df["date"], y=df["temperature"],
-                             labels={"x": "Date", "y": "Temperature (C)"})
+figure = px.line(x=date, y=temperature,
+                 labels={"x": "Date", "y": "Temperature (C)"})
 
 st.plotly_chart(figure)
+#text file date fetch
+# df = pandas.read_csv("data.txt")
+# figure = px.line(x=df["date"], y=df["temperature"],
+#                              labels={"x": "Date", "y": "Temperature (C)"})
+# st.plotly_chart(figure)
+
